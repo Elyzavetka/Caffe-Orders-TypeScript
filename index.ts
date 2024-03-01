4ff9eaee0ec1f955c8498d1f6f0208d5236b41ec
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-03-01 19:13:07 +0000

    Add function to calculate maximum price based on PriceBracket

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 import { coffeeShops, CoffeeShop } from "./coffeeShops";
 import { orders, Order, PriceBracket } from "./orders";
+
+const getMaxPrice = (price: PriceBracket) => {
+    switch (price) {
+        case PriceBracket.Low:
+            return 10;
+        case PriceBracket.Medium:
+            return 20;
+        case PriceBracket.High:
+            return 30;
+
+    }
+}
